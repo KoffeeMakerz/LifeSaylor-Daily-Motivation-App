0cf82ae44bc003a9c1989a5a2e489fbdd2ac77ba
Najeeb Weerabangsa <mohamadnajeeb39@gmail.com>
2023-12-09 08:59:04 +0530

    popup menu done

--- src/screens/MotivationTimeScreen/MotivationTimeScreen.js
+++ src/screens/MotivationTimeScreen/MotivationTimeScreen.js
@@ -1,21 +1,88 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, DatePickerAndroid, TimePickerAndroid } from 'react-native';
 
 const MotivationTimeScreen = () => {
+  const [showAddMotivationModal, setShowAddMotivationModal] = useState(false);
+  const [motivationQuote, setMotivationQuote] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState(new Date());
+
+  const openDatePicker = async () => {
+    try {
+      const { action, year, month, day } = await DatePickerAndroid.open({
+        date: selectedDate,
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        const newDate = new Date(year, month, day);
+        setSelectedDate(newDate);
+      }
+    } catch ({ code, message }) {
+      console.warn('Cannot open date picker', message);
+    }
+  };
+
+  const openTimePicker = async () => {
+    try {
+      const { action, hour, minute } = await TimePickerAndroid.open({
+        hour: selectedTime.getHours(),
+        minute: selectedTime.getMinutes(),
+        is24Hour: false,
+      });
+      if (action !== TimePickerAndroid.dismissedAction) {
+        const newTime = new Date();
+        newTime.setHours(hour, minute);
+        setSelectedTime(newTime);
+      }
+    } catch ({ code, message }) {
+      console.warn('Cannot open time picker', message);
+    }
+  };
+
+  const handleSaveMotivation = () => {
+    // Save the motivation and close the modal
+    // You can implement your saving logic here
+    setShowAddMotivationModal(false);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Motivation Time Settings</Text>
-      
+
       <View style={styles.addMotivationContainer}>
-        <Text style={styles.topic}>Add Your Motivation</Text>
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => setShowAddMotivationModal(true)}
+        >
           <Text style={styles.buttonText}>Add Motivation</Text>
         </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.saveButton}>
-        <Text style={styles.buttonText}>Save Settings</Text>
-      </TouchableOpacity>
+      {/* Add Motivation Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showAddMotivationModal}
+        onRequestClose={() => setShowAddMotivationModal(false)}
+      >
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalHeader}>Add Your Motivation</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your motivation quote"
+            value={motivationQuote}
+            onChangeText={(text) => setMotivationQuote(text)}
+          />
+          <TouchableOpacity style={styles.input} onPress={openDatePicker}>
+            <Text>Date: {selectedDate.toLocaleDateString()}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.input} onPress={openTimePicker}>
+            <Text>Time: {selectedTime.toLocaleTimeString()}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.saveButton} onPress={handleSaveMotivation}>
+            <Text style={styles.buttonText}>Save Motivation</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -35,17 +102,7 @@ const styles = StyleSheet.create({
   addMotivationContainer: {
     marginBottom: 20,
   },
-  topic: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
   addButton: {
-    backgroundColor: '#72c17d',
-    padding: 15,
-    borderRadius: 8,
-  },
-  saveButton: {
     backgroundColor: '#72c17d',
     padding: 15,
     borderRadius: 8,
@@ -54,6 +111,28 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  saveButton: {
+    backgroundColor: '#72c17d',
+    padding: 15,
+    borderRadius: 8,
   },
 });
 
